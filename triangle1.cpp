5ceb95f5efdd4343af88c75c6b1a717d675bf2b8
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-09-04 01:23:48 -0500

    Create triangle1.cpp

---
+++ triangle1.cpp
@@ -0,0 +1,28 @@
+/* Triangulo muestra la siguiente imagen 
+
+*********
+********
+*******
+******
+*****
+****
+***
+**
+*       */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+int i,j,n=9;
+int aux=n;
+for (j=1;j<=aux;j++){
+    
+for (i=1;i<=n;i++){
+    cout<<"*";
+   
+    }
+    cout<<endl;  n=n-1;}
+  
+}
