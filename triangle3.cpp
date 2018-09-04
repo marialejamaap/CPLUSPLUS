6607d167ac738cd77b65ba108823c10254d8b00a
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-09-04 01:54:27 -0500

    Create triangle3.cpp

---
+++ triangle3.cpp
@@ -0,0 +1,27 @@
+/* Imprime triangulo de la siguiente forma 
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********  */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i,j,n=9;
+    for(j=1;j<=n;j++){
+for (i=1;i<=j;i++)
+{cout<<"*";
+    }
+    cout<<endl;
+    
+}}
