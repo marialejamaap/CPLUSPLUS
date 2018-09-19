25468460acc2cd5fefe708d0156712907c8787e3
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-09-19 09:33:04 -0500

    Create lectura_de_datos.cpp

---
+++ lectura_de_datos.cpp
@@ -0,0 +1,13 @@
+// Lee datos de entrada
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    int numero_entero; // define el tipo y el nombre de la variable
+    cout<<"Digite un numero: "; // pide al usuario ingresar un numero
+    cin>> numero_entero; // ingrese la variable numero_entero que es de tipo entero definida anteriormente
+    // siempre se debe definir variable antes de ser utilizada
+    cout << "\nEl numero que usted ingreso fue: " << numero_entero; // muestra la variable --- \n --> equivale a un ---> endl
+    return 0; // retorna 0 a la funcion int main 
+    }
